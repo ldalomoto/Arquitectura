10022a4876b0a31fd3821aba174a012bb09dde8c
Lenin Alomoto <ldalomoto@uce.edu.ec>
2023-07-22 18:15:05 -0500

    grafica que muestra en tiempo real el rendimiento del CPU

---
+++ DocArq/js/grafico.js
@@ -0,0 +1,51 @@
+/*################################################################################################*/
+/*####################################### DESPLIEGUE DEL GRAFICO #################################*/
+/*################################################################################################*/
+
+const ctx = document.getElementById("myChart").getContext("2d");
+let labels_n = [];
+let data_n = [];
+let data_n2 = [];
+let data_n3 = [];
+let data_n4 = [];
+let myChart = new Chart(ctx, {
+	type: "line",
+	data: {
+		labels: labels_n,
+		datasets: [
+			{
+				label: "CPU",
+				data: data_n,
+				backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+				borderColor: ["rgba(255, 99, 132, 1)"],
+				borderWidth: 1,
+			},
+			{
+				label: "Base Line",
+				data: data_n2,
+				backgroundColor: ["rgba(0, 0, 255, 0.2)"],
+				borderColor: ["rgba(0, 0, 255,1)"],
+				borderWidth: 1,
+			}
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+});
+
+function addData(chart, dataS) {
+
+	let today = new Date();
+	let date =
+		today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	// Datos recolectados del sensor
+	chart.data.labels.push(date);
+chart.data.datasets[0].data.push(dataS);
+chart.data.datasets[1].data.push(40);
+	chart.update();
+}
